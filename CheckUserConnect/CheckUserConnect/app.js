28f14e516ae8dca62d3cd66750bed8081a3800ad
saeed <saeedasadsh@gmail.com>
2018-07-28 14:03:04 +0430

    123

--- CheckUserConnect/CheckUserConnect/app.js
+++ CheckUserConnect/CheckUserConnect/app.js
@@ -125,11 +125,13 @@ try {
         });
 
         socket.on('disconnect', function (data) {
+            delete Players[appId].players[playerId];
             console.log("disconnect in dis: " + playerId);
         });
 
         socket.on('close', function (data) {
             try {
+                delete Players[appId].players[playerId];
                 console.log("disconnect in close: " + playerId);
             }
             catch (e) {
